6a78111d864deb890ca436021e0255a8c7c16a91
Noelia Liquin <marianella.6ls@gmail.com>
2022-08-27 05:07:22 -0300

    ultimos cambios css

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ function onClick(event) {
 
 
 
-  const mensaje = { 
+  const mensaje = {
     comercio: document.getElementById('comercio').value,
-    titular: document.getElementById('titular').value,  
+    titular: document.getElementById('titular').value,
     cel: document.getElementById('cel').value
   }
 
@@ -21,15 +21,15 @@ function onClick(event) {
   })
 
     .then((response) => response.json())
-    
-    .then((json) => { 
 
-      console.log(json); 
-      Swal.fire( {//libreria swal :sweetalert2
-      title:'Registrado',
-      icon: 'success'
-       
-        }
+    .then((json) => {
+
+      console.log(json);
+      Swal.fire({
+        title: 'Registrado',
+        icon: 'success'
+
+      }
       );
 
       cleanForm();
@@ -42,8 +42,8 @@ function onClick(event) {
 }
 
 function cleanForm() {
-  let formulario = document.getElementById('formulario'); 
-  formulario.reset();   
+  let formulario = document.getElementById('formulario');
+  formulario.reset();
 }
 
 
@@ -52,22 +52,22 @@ boton.addEventListener("click", onClick);
 
 //Clima
 
-function getClima(){
-     var temp = document.querySelector(".temp");
-     var cloud = document.querySelector(".cloud");
-     var humidity = document.querySelector(".humidity");
+function getClima() {
+  var temp = document.querySelector(".temp");
+  var cloud = document.querySelector(".cloud");
+  var humidity = document.querySelector(".humidity");
 
-    fetch ("https://api.openweathermap.org/data/2.5/weather?lat=-24.183310024481678&lon=-65.33127108038181&appid=171f40ac237805ef34177d5adb547826")
+  fetch("https://api.openweathermap.org/data/2.5/weather?lat=-24.183310024481678&lon=-65.33127108038181&appid=171f40ac237805ef34177d5adb547826")
     .then(response => response.json())
     .then(data => {
       var tempValue = data["main"]["temp"];
       var cloudValue = data["weather"][0]["description"];
       var humidityValue = data["main"]["humidity"];
 
-      humidity.innerHTML = "Humedad: "+humidityValue+"º";
-      cloud.innerHTML = "Cielo: "+cloudValue;
-      temp.innerHTML ="Temperatura: "+(parseInt(Math.round(tempValue)-273))+"º";
-        })
-    .catch(err=> alert("Clima No disponible"));
-  }
-  getClima();
+      humidity.innerHTML = "Humedad: " + humidityValue + "º";
+      cloud.innerHTML = "Cielo: " + cloudValue;
+      temp.innerHTML = "Temperatura: " + (parseInt(Math.round(tempValue) - 273)) + "º";
+    })
+    .catch(err => alert("Clima No disponible"));
+}
+getClima();
