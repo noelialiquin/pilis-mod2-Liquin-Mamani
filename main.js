a7828384202b29a2b6c3fb81e342f98544f1e0b0
Dario Santiago Mamani <mamanidario.19@gmail.com>
2022-08-27 04:01:35 -0300

    agrego seccion clima

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-
-
-
-
-//          CLIMA
-
-
-
-
 //           FORMULARIO
 
 function onClick(event) {
@@ -58,3 +49,25 @@ function cleanForm() {
 
 let boton = document.getElementById("enviar");
 boton.addEventListener("click", onClick);
+
+//Clima
+
+function getClima(){
+     var temp = document.querySelector(".temp");
+     var cloud = document.querySelector(".cloud");
+     var humidity = document.querySelector(".humidity");
+
+    fetch ("https://api.openweathermap.org/data/2.5/weather?lat=-24.183310024481678&lon=-65.33127108038181&appid=171f40ac237805ef34177d5adb547826")
+    .then(response => response.json())
+    .then(data => {
+      var tempValue = data["main"]["temp"];
+      var cloudValue = data["weather"][0]["description"];
+      var humidityValue = data["main"]["humidity"];
+
+      humidity.innerHTML = "Humedad: "+humidityValue+"º";
+      cloud.innerHTML = "Cielo: "+cloudValue;
+      temp.innerHTML ="Temperatura: "+(parseInt(Math.round(tempValue)-273))+"º";
+        })
+    .catch(err=> alert("Clima No disponible"));
+  }
+  getClima();
